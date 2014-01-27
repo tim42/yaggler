2873fa7a7207a4b30c74f7a74bad34b09bb28d34
tim <feuill_t@epitech.eu>
2014-01-27 11:25:50 -0500

    [KLMB/YAGGLER]: Created the base header.

---
+++ yaggler/klmb/klmb.hpp
@@ -0,0 +1,44 @@
+//
+// file : klmb.hpp
+// in : file:///home/tim/projects/yaggler/yaggler/klmb/klmb.hpp
+//
+// created by : Timothée Feuillet on linux-coincoin.tim
+// date: 25/01/2014 11:01:45
+//
+//
+// Copyright (C) 2014 Timothée Feuillet
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef __N_16694418701490889448_861129517__KLMB_HPP__
+# define __N_16694418701490889448_861129517__KLMB_HPP__
+
+// ok, lets start with klmb, the "usable" YägGLer.
+
+namespace neam
+{
+  namespace klmb
+  {
+    namespace yaggler
+    {
+    } // namespace yaggler
+  } // namespace klmb
+} // namespace neam
+
+#endif /*__N_16694418701490889448_861129517__KLMB_HPP__*/
+
+// kate: indent-mode cstyle; indent-width 2; replace-tabs on; 
+
