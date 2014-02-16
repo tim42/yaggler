d203d58d62bfacef8a3b6dd4d4b1170bc654c482
tim <feuill_t@epitech.eu>
2014-02-16 12:57:43 -0500

    [YAGGLER/GEOMETRY/DRAWER]: introducing draw states. (drawers).

---
+++ yaggler/geometry/draw_state_base.hpp
@@ -0,0 +1,49 @@
+//
+// file : draw_state_base.hpp
+// in : file:///home/tim/projects/yaggler/yaggler/geometry/draw_state_base.hpp
+//
+// created by : Timothée Feuillet on linux-coincoin.tim
+// date: 20/01/2014 16:02:29
+//
+//
+// Copyright (C) 2013-2014 Timothée Feuillet
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+
+#ifndef __N_396478543633947865_1243388889__DRAW_STATE_BASE_HPP__
+# define __N_396478543633947865_1243388889__DRAW_STATE_BASE_HPP__
+
+namespace neam
+{
+  namespace yaggler
+  {
+    namespace geometry
+    {
+      // this class represent a 'draw state'.
+      template<typename DrawStateType, typename... Args>
+      class draw_state
+      {
+        static_assert(!(sizeof...(Args) + 1), "This draw state type doesn't exist or is not valid.");
+      };
+    } // namespace geometry
+  } // namespace yaggler
+} // namespace neam
+
+#endif /*__N_396478543633947865_1243388889__DRAW_STATE_BASE_HPP__*/
+
+// kate: indent-mode cstyle; indent-width 2; replace-tabs on; 
+
---
+++ yaggler/geometry/opengl_draw_state.hpp
@@ -0,0 +1,352 @@
+//
+// file : opengl_draw_state.hpp
+// in : file:///home/tim/projects/yaggler/yaggler/geometry/opengl_draw_state.hpp
+//
+// created by : Timothée Feuillet on linux-coincoin.tim
+// date: 15/02/2014 10:55:00
+//
+//
+// Copyright (C) 2014 Timothée Feuillet
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef __N_18391655302072008314_1562393302__OPENGL_DRAW_STATE_HPP__
+# define __N_18391655302072008314_1562393302__OPENGL_DRAW_STATE_HPP__
+
+#include <yaggler_type.hpp>
+#include <geometry/draw_state_base.hpp>
+
+namespace neam
+{
+  namespace yaggler
+  {
+    namespace geometry
+    {
+      namespace draw_type
+      {
+        class normal {};
+        class indexed {};
+        class instanced {};
+        class indexed_instanced {};
+      } // namespace draw_type
+
+
+      template<typename DrawType, typename... Args>
+      class draw_state<type::opengl, DrawType, Args...>
+      {
+        static_assert(!(sizeof...(Args) + 1), "This draw state draw_type doesn't exist or is not valid.");
+      };
+
+      template<>
+      class draw_state<type::opengl, draw_type::indexed>
+      {
+        public:
+          draw_state()
+            : mode(0), count(0), type(GL_UNSIGNED_INT), start_index(0)
+          {
+          }
+
+          void set_draw_triangles(size_t triangle_count)
+          {
+            mode = GL_TRIANGLES;
+            count = triangle_count * 3;
+          }
+          void set_draw_lines(size_t line_count)
+          {
+            mode = GL_LINE;
+            count = line_count * 2;
+          }
+          void set_draw_points(size_t point_count)
+          {
+            mode = GL_POINTS;
+            count = point_count;
+          }
+
+          void set_draw(GLenum _mode, size_t _count)
+          {
+            mode = _mode;
+            count = _count;
+          }
+
+          void set_index_type(GLenum _type)
+          {
+            type = _type;
+          }
+
+          void set_start_index(size_t _index)
+          {
+            start_index = _index;
+          }
+
+          GLenum get_mode() const
+          {
+            return mode;
+          }
+
+          size_t get_count() const
+          {
+            return count;
+          }
+
+          GLenum get_index_type() const
+          {
+            return type;
+          }
+
+          size_t get_start_index() const
+          {
+            return start_index;
+          }
+
+          void draw() const
+          {
+            glDrawElements(mode, count, type, reinterpret_cast<GLvoid *>(start_index));
+          }
+
+        public:
+          GLenum mode;
+          size_t count;
+          GLenum type;
+          size_t start_index;
+      };
+
+      template<>
+      class draw_state<type::opengl, draw_type::normal>
+      {
+        public:
+          draw_state()
+            : mode(0), start_index(0), count(0)
+          {
+          }
+
+          void set_draw_triangles(size_t triangle_count)
+          {
+            mode = GL_TRIANGLES;
+            count = triangle_count * 3;
+          }
+          void set_draw_lines(size_t line_count)
+          {
+            mode = GL_LINE;
+            count = line_count * 2;
+          }
+          void set_draw_points(size_t point_count)
+          {
+            mode = GL_POINTS;
+            count = point_count;
+          }
+
+          void set_draw(GLenum _mode, size_t _count)
+          {
+            mode = _mode;
+            count = _count;
+          }
+
+          void set_start_index(size_t _index)
+          {
+            start_index = _index;
+          }
+
+          GLenum get_mode() const
+          {
+            return mode;
+          }
+
+          size_t get_count() const
+          {
+            return count;
+          }
+
+          size_t get_start_index() const
+          {
+            return start_index;
+          }
+
+          void draw() const
+          {
+            glDrawArrays(mode, start_index, count);
+          }
+
+        public:
+          GLenum mode;
+          size_t start_index;
+          size_t count;
+      };
+
+
+      template<>
+      class draw_state<type::opengl, draw_type::instanced>
+      {
+        public:
+          draw_state()
+            : mode(0), count(0), start_index(0), prim_count(1)
+          {
+          }
+
+          void set_draw_triangles(size_t triangle_count)
+          {
+            mode = GL_TRIANGLES;
+            count = triangle_count * 3;
+          }
+          void set_draw_lines(size_t line_count)
+          {
+            mode = GL_LINE;
+            count = line_count * 2;
+          }
+          void set_draw_points(size_t point_count)
+          {
+            mode = GL_POINTS;
+            count = point_count;
+          }
+
+          void set_draw(GLenum _mode, size_t _count)
+          {
+            mode = _mode;
+            count = _count;
+          }
+
+          void set_start_index(size_t _index)
+          {
+            start_index = _index;
+          }
+
+          void set_primitive_count(size_t _prim_count)
+          {
+            prim_count = _prim_count;
+          }
+
+          GLenum get_mode() const
+          {
+            return mode;
+          }
+
+          size_t get_count() const
+          {
+            return count;
+          }
+
+          size_t get_primitive_count() const
+          {
+            return prim_count;
+          }
+
+          size_t get_start_index() const
+          {
+            return start_index;
+          }
+
+          void draw() const
+          {
+            glDrawArraysInstanced(mode, start_index, count, prim_count);
+          }
+
+        public:
+          GLenum mode;
+          size_t count;
+          size_t start_index;
+          size_t prim_count;
+      };
+
+            template<>
+      class draw_state<type::opengl, draw_type::indexed_instanced>
+      {
+        public:
+          draw_state()
+            : mode(0), count(0), type(GL_UNSIGNED_INT), start_index(0), prim_count(1)
+          {
+          }
+
+          void set_draw_triangles(size_t triangle_count)
+          {
+            mode = GL_TRIANGLES;
+            count = triangle_count * 3;
+          }
+          void set_draw_lines(size_t line_count)
+          {
+            mode = GL_LINE;
+            count = line_count * 2;
+          }
+          void set_draw_points(size_t point_count)
+          {
+            mode = GL_POINTS;
+            count = point_count;
+          }
+
+          void set_draw(GLenum _mode, size_t _count)
+          {
+            mode = _mode;
+            count = _count;
+          }
+
+          void set_index_type(GLenum _type)
+          {
+            type = _type;
+          }
+
+          void set_start_index(size_t _index)
+          {
+            start_index = _index;
+          }
+
+          void set_primitive_count(size_t _prim_count)
+          {
+            prim_count = _prim_count;
+          }
+
+          GLenum get_mode() const
+          {
+            return mode;
+          }
+
+          size_t get_count() const
+          {
+            return count;
+          }
+
+          GLenum get_index_type() const
+          {
+            return type;
+          }
+
+          size_t get_start_index() const
+          {
+            return start_index;
+          }
+
+          size_t get_primitive_count() const
+          {
+            return prim_count;
+          }
+
+          void draw() const
+          {
+            glDrawElementsInstanced(mode, count, type, reinterpret_cast<GLvoid *>(start_index), prim_count);
+          }
+
+        public:
+          GLenum mode;
+          size_t count;
+          GLenum type;
+          size_t start_index;
+          size_t prim_count;
+      };
+    } // namespace geometry
+  } // namespace yaggler
+} // namespace neam
+
+#endif /*__N_18391655302072008314_1562393302__OPENGL_DRAW_STATE_HPP__*/
+
+// kate: indent-mode cstyle; indent-width 2; replace-tabs on; 
+
