73d7295e5794c947780f4dd7a2790af9a51b7e79
tim <feuill_t@epitech.eu>
2014-02-18 15:36:11 -0500

    [YAGGLER/CMAKE]: libpng is required, link correctly with GLFW.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -16,5 +16,11 @@ endif(OPENGL_FOUND)
 #   message(FATAL_ERROR "could not find GLEW")
 # endif(GLEW_FOUND)
 
+find_package(PNG REQUIRED)
+
+
 # yaggler deps libs (for exec)
-set(YAGGLER_DEPS_LIBS ${OPENGL_gl_LIBRARY} glfw glew png)
+set(YAGGLER_DEPS_LIBS ${OPENGL_gl_LIBRARY} glfw glew ${PNG_LIBRARY} ${GLFW_STATIC_LIBRARIES} ${GLFW_LIBRARIES})
+
+# yaggler include dirs
+set(YAGGLER_INCLUDE_DIRS ${PNG_INCLUDE_DIR})
