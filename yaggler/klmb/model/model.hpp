a9df5eadf3c678b77c321a007419cdb25e1d4844
tim <feuill_t@epitech.eu>
2014-02-18 11:19:23 -0500

    [KLMB/MODEL/MODEL]: model is now working correctly

--- yaggler/klmb/model/model.hpp
+++ yaggler/klmb/model/model.hpp
@@ -42,24 +42,39 @@ namespace neam
       class model
       {
         public:
+          // the object geometry
+          object<> geometry;
+
 //           camera_holder *holder;
           glm::mat4 **vp_matrix = nullptr; // a pointer to the vp_matrix of the camera_holder.
           glm::mat4 *world_pos = nullptr;  // a pointer to the world_pos of the transformation_tree node.
 
-
-          // the object geometry
-          object<> geometry;
-
           // the material
           material_wrapper material;
 
+        public:
+          model(const object<> &_geometry, glm::mat4 **_vp_matrix = nullptr, glm::mat4 *_world_pos = nullptr, const material_wrapper &_material = material_wrapper())
+            : geometry(_geometry), vp_matrix(_vp_matrix), world_pos(_world_pos), material(_material) {}
+
+          model(object<> && _geometry, glm::mat4 **_vp_matrix = nullptr, glm::mat4 *_world_pos = nullptr, const material_wrapper & _material = material_wrapper())
+            : geometry(std::move(_geometry)), vp_matrix(_vp_matrix), world_pos(_world_pos), material((_material)) {}
+
+          model(object<> &&_geometry, glm::mat4 **_vp_matrix, glm::mat4 *_world_pos, material_wrapper &&_material)
+            : geometry(std::move(_geometry)), vp_matrix(_vp_matrix), world_pos(_world_pos), material(std::move(_material)) {}
+
+          model(const object<> & _geometry, glm::mat4 **_vp_matrix, glm::mat4 *_world_pos, material_wrapper && _material)
+            : geometry((_geometry)), vp_matrix(_vp_matrix), world_pos(_world_pos), material(std::move(_material)) {}
+
           void draw()
           {
-            material.get_object_matrix() = world_pos;
-            if (vp_matrix)
-              material.get_vp_matrix() = *vp_matrix;
-            material.use();
-            geometry.draw();
+            if (!material.is_empty())
+            {
+              material.get_object_matrix() = world_pos;
+              if (vp_matrix)
+                material.get_vp_matrix() = *vp_matrix;
+              material.use();
+              geometry.draw();
+            }
           }
 
         private:
