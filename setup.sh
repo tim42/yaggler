70c4fd170bbecd7db134f0776b68a3fa21096e7a
tim <timothee.feuillet@epitech.eu>
2014-11-09 15:19:55 +0100

    [YAGGLER/SETUP]: update of the setup script to take account of the repo change of the yaggler/tools subfolder

--- setup.sh
+++ setup.sh
@@ -1,86 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 #
-# a setup script for YagGLer
+# a (simple) setup script for YagGLer
 #
 
-# params
-use_submodules=0        # whether or not use git submodules
-show_help=0
 
-# extraction vars (command and archive extension)
-cmd=xz
-ext=xz
-
-# arguments
-if (( $# >= 1 ))
-then
-  case $1 in
-    '--use-git-submodules')
-      use_submodules=1
-    ;;
-    '--fetch-submodules')
-      use_submodules=0
-    ;;
-    '--help')
-      show_help=1
-    ;;
-    *)
-      echo 'unknow option "'$1'"...'
-      show_help=1
-    ;;
-  esac
-
-  if [ $show_help = 1 ]
-  then
-    echo "$0 options:"
-    echo "  --use-git-submodules : use a git submodule for neam/tools (need authentification)"
-    echo "  --fetch-submodules   : do not use a git submodules for neam/tools, but instead fetch the ftp server (default)"
-    exit 0
-  fi
-fi
-
-# fetch submodules from the ftp
-function fetch_and_extract # $1: project ftp path (eg: neam/tools), $2: base folder
-{
-  echo " -- fetching $1..."
-  ( cd "$2" ; curl -# "ftp://neam.co/projects/$1.tar.$ext" | $cmd -d | tar -x )
-}
-
-# setup submodules
-if [ $use_submodules = 0 ]
-then
-  echo " -- fetching subprojects from ftp://neam.co..."
-
-  if ! hash curl 2>/dev/null
-  then
-    echo 1>&2 " ** missing the curl command... :'("
-    exit 1
-  fi
-  if ! hash $cmd 2>/dev/null
-  then
-    cmd=bzip2
-    ext=bz2
-
-    if ! hash $cmd 2>/dev/null
-    then
-      echo 1>&2 " ** missing xz or bzip2 commands... :("
-      exit 1
-    fi
-  fi
-
-  echo " -- using $cmd to decompress the archives"
-  echo
-
-  # fetching and extracting modules
-  fetch_and_extract 'neam/tools' 'yaggler'
-
-else
-  echo " -- using git submodules..."
-  git submodule init
-  git submodule update
-fi
-
-echo " -- YagGLer is now setup"
-
+git submodule init
+git submodule update
 
 # EOF
